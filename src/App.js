d318dc9c761994e4e82ece2fc48b8c8828d14496
Lukas Bals <mail@lukasbals.at>
2019-12-09 18:30:47 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Culture Tickets Web Console</h1>
+      <Button>Ant Design Button</Button>
+    </Container>
   );
 }
 
